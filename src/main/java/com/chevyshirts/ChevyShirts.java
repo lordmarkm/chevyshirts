08571662d86febd76566c96f954b5d34dfe14d37
Mark Baldwin Martinez <mbmartinez@exist.com>
2015-12-21 19:58:39 +0800

    Initial commit

---
+++ src/main/java/com/chevyshirts/ChevyShirts.java
@@ -0,0 +1,13 @@
+package com.chevyshirts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChevyShirts {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChevyShirts.class, args);
+    }
+
+}
